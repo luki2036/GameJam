8a7a0807b59a06a77b490ad436544ea90647c0a3
Jan Kornfeld <kor16986@spengergasse.at>
2018-06-07 15:15:17 +0200

    new script + changes

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    public GameObject StartMenu;
+    public GameObject ModeMenu;
+    public GameObject PlayerMenu;
+    public GameObject obj4 ;
+    public GameObject obj5 ;
+    public GameObject obj6 ;
+    public GameObject obj7 ;
+    public GameObject obj8 ;
+    public GameObject obj9 ;
+    public GameObject obj10;
+    public GameObject obj11;
+    public GameObject obj12;
+    public GameObject obj13;
+
+	public void PlayButton()
+    {
+        StartMenu.SetActive(false);
+        ModeMenu.SetActive(true);
+    }
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+}
