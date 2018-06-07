cc79be2fdd680f05c3509fbd3334e8da30d2899f
Jan Kornfeld <kor16986@spengergasse.at>
2018-06-07 16:19:08 +0200

    Menu finished

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -7,25 +7,101 @@ public class Menu : MonoBehaviour {
     public GameObject StartMenu;
     public GameObject ModeMenu;
     public GameObject PlayerMenu;
-    public GameObject obj4 ;
-    public GameObject obj5 ;
-    public GameObject obj6 ;
-    public GameObject obj7 ;
-    public GameObject obj8 ;
-    public GameObject obj9 ;
-    public GameObject obj10;
-    public GameObject obj11;
-    public GameObject obj12;
-    public GameObject obj13;
+    public GameObject Player1Exit;
+    public GameObject Player1Char;
+    public GameObject Player2Exit;
+    public GameObject Player2Char;
+    public GameObject Player3Exit;
+    public GameObject Player3Char;
+    public GameObject Player4Exit;
+    public GameObject Player4Char;
+    //public GameObject 
+    //public GameObject 
 
-	public void PlayButton()
+    private void Start()
+    {
+        StartMenu.SetActive(true);
+        ModeMenu.SetActive(false);
+        PlayerMenu.SetActive(false);
+    }
+
+    public void PlayButton()
     {
         StartMenu.SetActive(false);
         ModeMenu.SetActive(true);
     }
-    public void ExitButton()
+
+    public void DeathBattle()
+    {
+        ModeMenu.SetActive(false);
+        PlayerMenu.SetActive(true);
+    }
+    public void ExitGameButton()
     {
         Application.Quit();
+    }
+
+    public void JoinButton(int player)
+    {
+        if (player == 1)
+        {
+            Player1Exit.SetActive(true);
+            Player1Char.SetActive(true);
+        }
+        else if(player == 2)
+        {
+            Player2Exit.SetActive(true);
+            Player2Char.SetActive(true);
+        }
+        else if(player == 3)
+        {
+            Player3Exit.SetActive(true);
+            Player3Char.SetActive(true);
+        }
+        else if(player == 4)
+        {
+            Player4Exit.SetActive(true);
+            Player4Char.SetActive(true);
+        }
+    }
+
+    public void ExitButton(int player)
+    {
+        if (player == 1)
+        {
+            Player1Exit.SetActive(false);
+            Player1Char.SetActive(false);
+        }
+        else if (player == 2)
+        {
+            Player2Exit.SetActive(false);
+            Player2Char.SetActive(false);
+        }
+        else if (player == 3)
+        {
+            Player3Exit.SetActive(false);
+            Player3Char.SetActive(false);
+        }
+        else if (player == 4)
+        {
+            Player4Exit.SetActive(false);
+            Player4Char.SetActive(false);
+        }
+    }
+
+    public void Back(int screen)
+    {
+        if(screen == 1)
+        {
+            StartMenu.SetActive(true);
+            ModeMenu.SetActive(false);
+        }
+        else
+        {
+            PlayerMenu.SetActive(false);
+            ModeMenu.SetActive(true);
+        }
+        
     }
 
 }
