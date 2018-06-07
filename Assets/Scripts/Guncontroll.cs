05339a8530f6c74f468b8683dacfae5641ae28d4
Katic Lukas <kat16977@spengergasse.at>
2018-06-07 12:00:59 +0200

    Something

--- Assets/Scripts/Guncontroll.cs
+++ Assets/Scripts/Guncontroll.cs
@@ -1,29 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Guncontroll : MonoBehaviour {
+public class Guncontroll : NetworkBehaviour {
 
-    public Transform aim;
+    Transform aim;
     public GameObject bullet;
     public Transform gunExit;
     bool canShoot;
 
     private void Start()
     {
+        aim = GameObject.Find("AimTemp").GetComponent<Transform>();
         canShoot = true;    
     }
 
     void Update()
     {
+        if (!this.GetComponentInParent<Playercontrolls>().isLocalPlayer)
+            return;
+
         transform.LookAt(aim);
 
         if (Input.GetButton("Fire1") && canShoot)
         {
-            GameObject.Instantiate(bullet, gunExit.position, new Quaternion());
+            CmdShoot(this.GetComponentInParent<Rigidbody2D>(), aim, this.GetComponentInParent<Transform>(), gunExit, bullet);
             canShoot = false;
             StartCoroutine(waitForNextShot());
         }
+    }
+
+    
+    //[Command]
+    public void CmdShoot(Rigidbody2D playerrb, Transform aim, Transform player, Transform gunExit, GameObject bullet)
+    {
+        var spawning = GameObject.Instantiate(bullet, gunExit.position, new Quaternion());
+        Rigidbody2D rb = spawning.GetComponent<Rigidbody2D>();
+        rb.velocity = (aim.position - player.position) * 0.7f;
+        rb.velocity += (playerrb.velocity * 0.7f);
+        NetworkServer.Spawn(spawning);
     }
 
     IEnumerator waitForNextShot()
