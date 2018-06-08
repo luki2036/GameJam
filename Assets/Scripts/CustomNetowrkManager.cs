483ed78e9d367e35165eebb01402c6c0df932a99
Katic Lukas <kat16977@spengergasse.at>
2018-06-08 12:24:04 +0200

    Platform is WEIS

---
+++ Assets/Scripts/CustomNetowrkManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class CustomNetowrkManager : NetworkManager {
+
+	public void HostDeathbattle()
+    {
+        SetPort(7777);
+        NetworkManager.singleton.StartHost();
+    }
+
+    public void JoinDeathbattle()
+    {
+        SetIPAddress();
+        SetPort(7777);
+        NetworkManager.singleton.StartClient();
+    }
+
+    public void HostWIP()
+    {
+        SetPort(7776);
+        NetworkManager.singleton.StartHost();
+    }
+
+    public void JoinWIP()
+    {
+        SetPort(7776);
+        SetIPAddress();
+        NetworkManager.singleton.StartHost();
+    }
+
+
+    void SetIPAddress()
+    {
+        NetworkManager.singleton.networkAddress = "127.0.0.1";
+    }
+
+    void SetPort(int port)
+    {
+        NetworkManager.singleton.networkPort = port;
+    }
+}
